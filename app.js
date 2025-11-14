73ca317550cb7ffd0b3255c66416012cd0608524
dantearona-collab <dante.arona@gmail.com>
2025-11-14 12:07:47 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -222,7 +222,7 @@ async function loadProperties() {
     try {
         console.log('📂 Cargando datos de propiedades...');
         
-        const response = await fetch('imgs/propiedades.json');
+        const response = await fetch('propiedades.json');
         if (!response.ok) {
             throw new Error(`Error HTTP! status: ${response.status}`);
         }
