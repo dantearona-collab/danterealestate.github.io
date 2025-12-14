380d502a29546a9dc6b65da40d1390856c907def
dantearona-collab <dante.arona@gmail.com>
2025-12-14 19:20:05 -0300

    Add files via upload

---
+++ logic/filter_data.py
@@ -0,0 +1,38 @@
+
+# -*- coding: utf-8 -*-
+"""
+Archivo central para almacenar las listas de filtros estáticos.
+"""
+
+# Lista de barrios disponibles.
+# Extraída de properties.json para asegurar consistencia.
+BARRIOS = [
+    "Parque Avellaneda",
+    "Boedo",
+    "Microcentro",
+    "Pilar",
+    "Colegiales",
+    "Palermo",
+    "Belgrano",
+    "Recoleta",
+    "Almagro",
+    "Villa Crespo",
+    "San Isidro",
+    "Vicente Lopez"
+]
+
+# Lista de operaciones de propiedad disponibles.
+OPERACIONES = [
+    "venta",
+    "alquiler"
+]
+
+# Lista de tipos de propiedad disponibles.
+TIPOS = [
+    "casa",
+    "terreno",
+    "departamento",
+    "oficina",
+    "ph",
+    "casaquinta"
+]
